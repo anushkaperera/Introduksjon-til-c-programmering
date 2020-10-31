5dfa3e7921605be08a872ca16b2e4dc3983a51aa
Anushka Perera <anushka.uom.tuc@gmail.com>
2020-11-01 00:11:57 +0100

    Create c-leksjon-3.c

---
+++ c-leksjon-3/c-leksjon-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hei\n");
+	printf("Jeg liker C\n");
+	printf("Jeg er Tom\n");
+	printf("Jeg er %d år gammel\n", 38);
+	printf("=======================\n");
+	printf("Jer er %s, er %d år gammel og veier %f\n", "Tom", 38, 70.5);
+	printf("=======================\n");
+	return 0;
+}
